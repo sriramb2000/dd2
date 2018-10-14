2b77e0a559c7196c0ef8166a7ea37a451dbe580b
bagpipes <dylanswafflez@gmail.com>
2018-10-14 00:42:54 -0700

    pretty

--- src/App.js
+++ src/App.js
@@ -32,11 +32,11 @@ class App extends Component {
     if(this.props.user){
       return (
         <div className="App">
-          <header className="App-header">
+          <header className="App-header2">
             <div className="page">
               <UserData user={this.props.user}/>
               <br/>
-              <button className="but" onClick={signOut}>SIGNOUT</button>
+              <button className="but0" onClick={signOut}>SIGNOUT</button>
             </div>
           </header>
 
@@ -45,15 +45,13 @@ class App extends Component {
     } else {
       return (
       <div className="App">
-        <header className="App-header">
-          <img className="circle" src="/circles.png" width="400x" height="400px" />
+        <header className="App-header1">
+          <div className="circ"></div>
           <div className="double">
             <h1>dd2</h1>
             <img className="btn" onClick={signInWithGoogle} src="btn.png"/>
-
           </div>
         </header>
-
       </div>
       );
     }
@@ -71,10 +69,13 @@ class UserData extends Component {
       var frequency = FREQUENCY;
       this.state = {
         "mic": mic,
+        micon: false,
         text,
-        frequency
+        frequency,
       };
-      this.handleClick = this.handleClick.bind(this);
+      this.handleClickStat = this.handleClickStat.bind(this);
+      this.handleClickVar = this.handleClickVar.bind(this);
+      this.handleClickRec = this.handleClickRec.bind(this);
       this.handleToggle = this.handleToggle.bind(this);
       this.userExistsCallback = this.userExistsCallback.bind(this);
     }
@@ -84,22 +85,36 @@ class UserData extends Component {
     var usersRef = firebase.database().ref('data/'+this.props.user.uid);
     usersRef.child('frequency').once('value', (snapshot) => {
       var exists = (snapshot.val() !== null);
-      console.log('snapshot');
       this.userExistsCallback(this.props.user.uid, exists);
     });
   }
 
-  handleClick(){
+  handleClickStat(){
+    var synth = new Tone.Synth().toMaster();
+
+    if (!Tone.UserMedia.supported){
+      console.log('nein');
+    } else {
+      synth.triggerAttackRelease("C4", 100);
+      this.setState({
+        "text": "Emitting at " + this.state.frequency + "Hz"
+      });
+    }
+  }
+
+  handleClickVar(){
     var synth = new Tone.Synth().toMaster();
 
     if (!Tone.UserMedia.supported){
           console.log('nein');
         } else {
-          synth.triggerAttackRelease('G4', '8n');
+          synth.triggerAttackRelease('G4', '1n');
 
           console.log(this.state.mic);
+          this.setState({micon:true});
+          console.log(this.state.micon);
           this.state.mic.open().then(function(){
-            console.log("lofo");
+            
           });
           this.setState({
             "text": "Emitting at " + this.state.frequency + "Hz"
@@ -107,17 +122,21 @@ class UserData extends Component {
     }
   }
 
+  handleClickRec(){
+
+  }
+
   handleToggle(){
     console.log("tog");
+
     this.state.mic.close();
-    var text = ""
+    var text = "";
     this.setState({
       text
     });
   }
 
   userExistsCallback(userId, exists){
-    console.log('bob');
     if (!exists) {
       console.log("don'texist");
       firebase.database().ref('data/'+userId+'/frequency').set({
@@ -130,9 +149,43 @@ class UserData extends Component {
     return (
       <div>
         <h1 className="name">{this.props.user.displayName}</h1>
-        <p>{this.state.text}</p>
-        <button className="but" onClick={this.handleClick}>send radio</button>
-        <button className="but" onClick={this.handleToggle}>close</button>
+        <div className="inner">
+          <div className="h1">
+            <span>
+            frequency (power) : &nbsp;
+            </span>
+            <select className="sel" name="cars">
+              <option value="250">250</option>
+              <option value="300">300</option>
+              <option value="350">350</option>
+              <option value="400">400</option>
+              <option value="450">450</option>
+            </select>
+            <span> mHz &nbsp;</span>
+            <button className="but" onClick={this.handleClickStat}>send radio</button> <br/>
+
+            <button className="but2" onClick={this.handleClickVar}>send variable radio</button> <br/>
+          </div>
+          <div className="h2">
+            <span>
+            frequency (power) : &nbsp;
+            </span>
+            <select className="sel" name="cars">
+              <option value="250">250</option>
+              <option value="300">300</option>
+              <option value="350">350</option>
+              <option value="400">400</option>
+              <option value="450">450</option>
+            </select>
+            <span> mHz &nbsp;</span>
+            <button className="but" onClick={this.handleClickRec}>receive radio</button>
+            <hr className="rule"/>
+            <button className="but3 marg" onClick={this.handleToggle}>close radio</button>
+            <div className="log">
+              {this.state.text}
+            </div>
+          </div>
+        </div>
       </div>
     );
   }
